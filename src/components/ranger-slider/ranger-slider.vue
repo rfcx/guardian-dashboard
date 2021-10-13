80a9302f9352aeebaec78e24f78d2428b706b9fa
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2021-10-13 16:11:42 +0300

    fix lint issues

--- src/components/ranger-slider/ranger-slider.vue
+++ src/components/ranger-slider/ranger-slider.vue
@@ -24,13 +24,13 @@
                 </a>
               </div>
             </div>
-            <div v-if="isLoading" class="flex flex-row bg-steel-grey h-10 w-10 min-h-lg" v-bind:style="{margin: [0, 'auto']}">
+            <div v-if="isLoading" class="flex flex-row bg-steel-grey h-10 w-10 min-h-lg" v-bind:style="{margin: '0 auto'}">
               <svg class="animate-spin bg-steel-grey text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
               </svg>
             </div>
-            <div v-if="!isLoading && images.length" class="flex flex-row min-h-lg items-center justify-between bg-no-repeat bg-center bg-contain" v-bind:style="{'background-image': 'url(' + images[currentIndex] + ')'}">
+            <div v-if="!isLoading && images.length" class="flex flex-row min-h-lg items-center justify-between" v-bind:style="{'background': 'center / contain no-repeat url(' + images[currentIndex] + ')'}">
               <a class="cursor-pointer" v-if="images.length > 1" @click="getPrevImage()" title="Previous Image">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
