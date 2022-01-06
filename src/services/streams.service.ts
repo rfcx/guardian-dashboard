2fe4153d62d8ddd4e0b5a8fb489d564f4de7c159
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2022-01-06 17:41:48 +0300

    #47 Fixed params logic in the streams endpoint

--- src/services/streams.service.ts
+++ src/services/streams.service.ts
@@ -3,13 +3,7 @@ import { Stream } from '@/types'
 import ApiClient from './api.service'
 
 export async function getStreams (projects?: string[], keyword?: string): Promise<{ data: Stream[], headers: any }> {
-  Endpoints.getStreams.config = {
-    params: {
-      ...projects !== undefined && { projects: projects },
-      ...keyword !== undefined && { keyword: keyword },
-      limit: 10000
-    }
-  }
+  Endpoints.getStreams.config = { params: { projects, keyword, limit: 10000 } }
   try {
     const resp = await ApiClient.request<Stream[]>({
       ...Endpoints.getStreams
