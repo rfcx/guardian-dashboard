37d037bff3ab992bf7b7f8b0ff567338a6ff9801
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2022-01-28 22:37:21 +0300

    The player component is updated

--- src/components/ranger-player-modal/ranger-player-modal.vue
+++ src/components/ranger-player-modal/ranger-player-modal.vue
@@ -1,146 +1,51 @@
 <template>
   <div
-    id="ranger-player-modal-component"
-    class="fixed inset-0 z-10 bg-mirage-grey bg-opacity-75 transition-opacity overflow-y-auto h-full w-full"
-    role="dialog"
-    aria-modal="true"
-    aria-labelledby="modal-title"
+    id="player"
+    class="bg-mirage-grey bg-opacity-75 rounded-lg px-2 py-1 flex flex-row items-center h-30px"
   >
-    <div class="flex justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-      <span
-        class="hidden sm:inline-block sm:align-middle sm:h-screen"
-        aria-hidden="true"
+    <button
+      class="cursor-pointer relative z-1 focus:outline-none mr-2"
+      :title="isPlaying? 'Stop' : 'Play'"
+      @click="toggleSound()"
+    >
+      <img
+        v-if="!isLoading && isPlaying"
+        class="inline-block"
+        src="/src/assets/pause.svg"
+        alt=""
+      >
+      <img
+        v-if="!isLoading && !isPlaying"
+        class="inline-block"
+        src="/src/assets/play.svg"
+        alt=""
+      >
+      <img
+        v-if="isLoading"
+        class="h-2.5 w-2.5 animate-spin bg-mirage-grey text-white"
+        src="/src/assets/spinner.svg"
+      >
+    </button>
+    <div class="text-gray-500 mr-2">
+      <span class="text-xs">{{ audioProp.current }}</span>
+      /
+      <span class="text-xs">{{ audioProp.total }}</span>
+    </div>
+    <div
+      v-if="!isError"
+      class="w-100px rounded-full h-2.5 bg-gray-600 mr-2"
+      @click="onProgressClicked($event)"
+    >
+      <div
+        class="bg-gray-300 h-2.5 rounded-full"
+        :style="{'width': progress + '%'}"
       />
-      <div class="inline-block bg-steel-grey rounded text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-2xl sm:w-full">
-        <OnClickOutside @trigger="close">
-          <div>
-            <div class="flex flex-row justify-between content-center pt-4 pb-2 px-4 border-b-1">
-              <h1 class="text-xl text-white">
-                Play Sound
-              </h1>
-              <a
-                class="cursor-pointer"
-                @click="close()"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div
-            v-if="isLoading || !audioProp"
-            class="flex flex-row bg-steel-grey h-10 w-10 min-h-lg"
-            :style="{margin: '0 auto'}"
-          >
-            <svg
-              class="animate-spin bg-steel-grey text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                class="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                stroke-width="4"
-              />
-              <path
-                class="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              />
-            </svg>
-          </div>
-          <div
-            v-if="!isLoading"
-            class="flex flex-row min-h-lg items-center justify-center relative overflow-hidden"
-          >
-            <div
-              class="absolute w-full h-full left-0 right-0 z-0 opacity-40"
-              style="background: no-repeat url('/src/assets/microphone.svg'); background-size: 60%; background-position: 118% 82%;"
-            />
-            <button
-              class="cursor-pointer relative z-1 focus:outline-none"
-              :title="isPlaying? 'Stop' : 'Play'"
-              @click="toggleSound()"
-            >
-              <svg
-                v-if="!isPlaying"
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-10 w-10"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <svg
-                v-if="isPlaying"
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-10 w-10"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </button>
-          </div>
-          <div class="flex justify-end px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <button
-              v-if="!isError"
-              :class="{'opacity-50 cursor-not-allowed': isDownloading}"
-              :disabled="isDownloading"
-              class="btn btn-primary"
-              @click="downloadAssets()"
-            >
-              <img
-                class="h-5 w-5 inline-block align-top mr-0.5"
-                src="/src/assets/download.svg"
-                alt=""
-              >
-              Download
-            </button>
-            <span
-              v-else
-              class="text-sm font-medium text-white px-3 py-2.5"
-              :class="{'text-danger': isError}"
-            >
-              Error occurred
-            </span>
-          </div>
-        </OnClickOutside>
-      </div>
+    </div>
+    <div
+      v-if="isError"
+      class="w-100px text-xs px-2"
+    >
+      Error occured
     </div>
   </div>
 </template>
