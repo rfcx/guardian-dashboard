08950ee316785f8c6154eec27a95703b7a8dcdfa
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2022-03-27 20:53:08 +0300

    let localStorage: Storage is removed

--- src/locals/i18n.ts
+++ src/locals/i18n.ts
@@ -6,19 +6,14 @@ import ind from './in.json'
 type MessageSchema = typeof en
 
 export const getLocalLang = (): string | null => {
-  try {
-    const lang = localStorage.getItem('GDLang')
-    if (lang !== null) {
-      return lang
-    }
-    if (['in_ID', 'in'].includes(navigator.language)) {
-      return 'in'
-    }
-    return 'en'
-  } catch (e) {
-    console.log(e)
-    return null
+  const lang = localStorage.getItem('GDLang')
+  if (lang !== null) {
+    return lang
   }
+  if (['in_ID', 'in'].includes(navigator.language)) {
+    return 'in'
+  }
+  return 'en'
 }
 
 const i18n = createI18n<I18nOptions, [MessageSchema], 'en', 'in'>({
